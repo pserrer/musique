72ab0e2112e4908ea1670c458f1d3d7056f6ecfe
Philipp Serrer <philipp@serrer.de>
2017-02-02 16:03:00 +0100

    Cleanup of Input Controller Script

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections.Generic;
 using System.Linq;
 using Assets.Scripts;
@@ -19,68 +17,30 @@ public class InputController : MonoBehaviour
 
 	public float SilenceBetweenMelodies = 8; // Seconds
 	public float MaxTimeBetweenNotesForChord = 0.05f;
-
-	public ParticleSystem DefaultSystem;
-
-	public Transform Parent;
-	private MidiDriver _midiDriver;
+	
 
 	// Use this for initialization
 	void Start()
 	{
+
 		_keys = Enumerable.Range((int)KeyCode.A, 26).Select(x => (KeyCode)x);
 		_currentMelody = new Melody {Start = Time.time};
-		_midiDriver = MidiDriver.Instance;
 
 		MidiMaster.noteOnDelegate += (channel, midiNote, velocity) =>
 		{
-			var octave = 0;
-			var note = MidiNoteToNote(midiNote, out octave);
-			NoteDown(note, octave);
+			var note = MelodyNote.FromMidiNote(midiNote);
+			note.Velocity = velocity;
+			NoteDown(note);
 		};
 
-		MidiMaster.noteOnDelegate += (channel, midiNote, velocity) =>
+		MidiMaster.noteOffDelegate += (channel, note) =>
 		{
-			var octave = 0;
-			var note = MidiNoteToNote(midiNote, out octave);
-			NoteUp(note, octave);
+			var midiNote = MelodyNote.FromMidiNote(note);
+			NoteUp(midiNote);
 		};
 
 	}
 
-	protected Note MidiNoteToNote(int note, out int octave)
-	{
-		octave = note/12;
-		var noteInOctave = note - octave*12;
-		switch (noteInOctave)
-		{
-			case 0:
-				return new Note("C");
-			case 1:
-				return new Note("C#");
-			case 2:
-				return new Note("D");
-			case 3:
-				return new Note("D#");
-			case 4:
-				return new Note("E");
-			case 5:
-				return new Note("F");
-			case 6:
-				return new Note("F#");
-			case 7:
-				return new Note("G");
-			case 8:
-				return new Note("G#");
-			case 9:
-				return new Note("A");
-			case 10:
-				return new Note("A#");
-			case 11:
-				return new Note("B");
-		}
-		throw new Exception("Invalid Note");
-	}
 
 	// Update is called once per frame
 	void Update()
@@ -90,29 +50,26 @@ public class InputController : MonoBehaviour
 			// Keyboard Down
 			if (Input.GetKeyDown(keyCode))
 			{
-				NoteDown(FromKeyCode(keyCode));
+				NoteDown(MelodyNote.FromKeyCode(keyCode));
 			}
 
 			// Keyboard Up
 			if (Input.GetKeyUp(keyCode))
 			{
-				NoteUp(FromKeyCode(keyCode));
+				NoteUp(MelodyNote.FromKeyCode(keyCode));
 			}
 		}
 	}
 
-	private void NoteUp(Note midiNote, int octave = 4)
+	private void NoteUp(MelodyNote note)
 	{
-		var key = midiNote.PositionInOctave + octave*12;
-		if (!_activeNotes.ContainsKey(key))
+		if (!_activeNotes.ContainsKey(note.GetHashCode()))
 		{
 			return;
 		}
 
-		var note = _activeNotes[key];
 		note.Duration = Time.time - note.Start;
-		StartCoroutine(RemoveParticleSystem(note.ParticleSystem, 5f));
-		_activeNotes.Remove(key);
+		_activeNotes.Remove(note.GetHashCode());
 
 		var chord = _lastNote as MelodyChord;
 		if (chord != null)
@@ -122,7 +79,7 @@ public class InputController : MonoBehaviour
 		}
 	}
 
-	private void NoteDown(Note midiNote, int octave = 4)
+	private void NoteDown(MelodyNote note)
 	{
 		// Start a new Melody, because pause was long enough
 		if (Time.time - _lastInput > SilenceBetweenMelodies)
@@ -131,26 +88,10 @@ public class InputController : MonoBehaviour
 		}
 
 		_lastInput = Time.time;
-		var key = midiNote.PositionInOctave + octave*12;
-		if (!_activeNotes.ContainsKey(key))
+		if (!_activeNotes.ContainsKey(note.GetHashCode()))
 		{
-			var note = new MelodyNote(midiNote.Letter, midiNote.Accidental)
-			{
-				Start = _currentMelody.Start,
-				Octave = octave,
-				Note = midiNote
-			};
-
-			var system = (ParticleSystem) Instantiate(DefaultSystem, Vector3.zero, Quaternion.identity);
-			ModifyParticleSystem(system);
-			system.Play();
-
-			note.ParticleSystem = system;
-
-			system.transform.position = PositionForNote(note);
-
-			_activeNotes.Add(key, note);
-			system.transform.parent = Parent;
+			note.Start = Time.time - _currentMelody.Start;
+			_activeNotes.Add(note.GetHashCode(), note);
 
 			// Group Notes into chord
 			if (_lastNote != null)
@@ -162,8 +103,7 @@ public class InputController : MonoBehaviour
 					{
 						chord = new MelodyChord()
 						{
-							Start = _lastNote.Start,
-							ParticleSystem = null // Maybe add an extra effect for chord?
+							Start = _lastNote.Start
 						};
 						// replace last entry with chord
 						_currentMelody.Notes.Remove(_lastNote);
@@ -187,74 +127,6 @@ public class InputController : MonoBehaviour
 				_lastNote = note;
 			}
 		}
-	}
-
-
-	private static Note FromKeyCode(KeyCode key)
-	{
-		switch (key)
-		{
-			case KeyCode.A:
-				return new Note('C');
-			case KeyCode.W:
-				return new Note('C', 1);
-			case KeyCode.S:
-				return new Note('D');
-			case KeyCode.E:
-				return new Note('D', 1);
-			case KeyCode.D:
-				return new Note('E');
-			case KeyCode.F:
-				return new Note('F');
-			case KeyCode.T:
-			case KeyCode.R:
-				return new Note('F', 1);
-			case KeyCode.G:
-				return new Note('G');
-			case KeyCode.Z:
-				return new Note('G', 1);
-			case KeyCode.H:
-				return new Note('A');
-			case KeyCode.U:
-				return new Note('A', 1);
-			case KeyCode.J:
-				return new Note('B');
-		}
-
-		// Invalid Input
-		throw new Exception();
-	}
-
-	private Vector3 PositionForNote(MelodyNote note)
-	{
-		var speed = 10f;
-		var radius = 5f;
-		var center = Parent.transform.position;
-		var rotation = Quaternion.Euler(0, Time.time * speed % 360, 0);
-		return center + rotation * (Vector3.forward * radius);
-	}
-
-	private static void ModifyParticleSystem(ParticleSystem ps)
-	{
-		var col = ps.colorBySpeed;
-		col.enabled = true;
-
-		Gradient grad = new Gradient();
-		grad.SetKeys(new GradientColorKey[] { new GradientColorKey(Color.blue, 0.0f), new GradientColorKey(Color.red, 1.0f) }, new GradientAlphaKey[] { new GradientAlphaKey(1.0f, 0.0f), new GradientAlphaKey(0.0f, 1.0f) });
-
-		col.color = new ParticleSystem.MinMaxGradient(grad);
-
-	}
-
-	protected IEnumerator RemoveParticleSystem(ParticleSystem system, float wait)
-	{
-		system.Stop();
-		if (wait > 0)
-		{
-			yield return new WaitForSeconds(wait);
-		}
-
-		Destroy(system.gameObject);
 	}
 
 	private void StartNewMelody()
--- Assets/Scripts/MelodyNote.cs
+++ Assets/Scripts/MelodyNote.cs
@@ -6,21 +6,120 @@ namespace Assets.Scripts
 	[Serializable]
 	public class MelodyNote
 	{
+		public const int DefaultOctave = 4;
 
-		public Note Note;
+		public char Letter { get; set; }
 
-		public float Start;
+		public int Accidental { get; set; }
 
-		public float Duration;
+		public int Octave { get; set; }
 
-		public ParticleSystem ParticleSystem;
+		public float Start { get; set; }
 
-		public MelodyNote(char letter, int accidental = 0)
+		public float Duration { get; set; }
+
+		public float Velocity { get; set; }
+
+		public MelodyNote(char letter, int accidental = 0, int octave = DefaultOctave)
 		{
-			Note = new Note(letter, accidental);
+			Letter = letter;
+			Accidental = accidental;
+			Octave = octave;
 		}
 
 		public MelodyNote() { }
-		public int Octave { get; set; }
+
+		protected bool Equals(MelodyNote other)
+		{
+			return Letter == other.Letter && Accidental == other.Accidental && Octave == other.Octave;
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (ReferenceEquals(null, obj)) return false;
+			if (ReferenceEquals(this, obj)) return true;
+			if (obj.GetType() != this.GetType()) return false;
+			return Equals((MelodyNote) obj);
+		}
+
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				var hashCode = Letter.GetHashCode();
+				hashCode = (hashCode*397) ^ Accidental;
+				hashCode = (hashCode*397) ^ Octave;
+				return hashCode;
+			}
+		}
+
+		public static MelodyNote FromMidiNote(int note)
+		{
+			var octave = note / 12;
+			var noteInOctave = note - octave * 12;
+			switch (noteInOctave)
+			{
+				case 0:
+					return new MelodyNote('C', 0, octave);
+				case 1:
+					return new MelodyNote('C', 1, octave);
+				case 2:
+					return new MelodyNote('D', 0, octave);
+				case 3:
+					return new MelodyNote('D', 1, octave);
+				case 4:
+					return new MelodyNote('E', 0, octave);
+				case 5:
+					return new MelodyNote('F', 0, octave);
+				case 6:
+					return new MelodyNote('F', 1, octave);
+				case 7:
+					return new MelodyNote('G', 0, octave);
+				case 8:
+					return new MelodyNote('G', 1, octave);
+				case 9:
+					return new MelodyNote('A', 0, octave);
+				case 10:
+					return new MelodyNote('A', 1, octave);
+				case 11:
+					return new MelodyNote('B', 0, octave);
+			}
+			throw new Exception("Invalid Note");
+		}
+
+		public static MelodyNote FromKeyCode(KeyCode key)
+		{
+			switch (key)
+			{
+				case KeyCode.A:
+					return new MelodyNote('C');
+				case KeyCode.W:
+					return new MelodyNote('C', 1);
+				case KeyCode.S:
+					return new MelodyNote('D');
+				case KeyCode.E:
+					return new MelodyNote('D', 1);
+				case KeyCode.D:
+					return new MelodyNote('E');
+				case KeyCode.F:
+					return new MelodyNote('F');
+				case KeyCode.T:
+				case KeyCode.R:
+					return new MelodyNote('F', 1);
+				case KeyCode.G:
+					return new MelodyNote('G');
+				case KeyCode.Z:
+					return new MelodyNote('G', 1);
+				case KeyCode.H:
+					return new MelodyNote('A');
+				case KeyCode.U:
+					return new MelodyNote('A', 1);
+				case KeyCode.J:
+					return new MelodyNote('B');
+			}
+
+			// Invalid Input
+			throw new Exception();
+		}
 	}
 }
