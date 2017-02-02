3c8da6fd67aa7ac2c2bab358433e33666e95a05b
Philipp Serrer <philipp@serrer.de>
2017-02-02 16:06:19 +0100

    ParticleController for placing the ParticleTargets on the scene and controlling their behavior

---
+++ Assets/ParticleController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleController : MonoBehaviour
+{
+
+	public Vector3 Center;
+	public Vector3 ParticleOffset;
+
+	public ParticleTarget ParticleTarget;
+	public int TargetCount = 5;
+	public float Radius = 20f;
+
+	private List<ParticleTarget> _targets;
+
+	// Use this for initialization
+	void Start () {
+		_targets = new List<ParticleTarget>();
+		for (var i = 0; i < TargetCount; i++)
+		{
+			var angle = ((float)i)/TargetCount*360;
+			var position = Center + Quaternion.AngleAxis(angle, Vector3.up) * (Vector3.forward*Radius) + ParticleOffset;
+			var target = (ParticleTarget) Instantiate(ParticleTarget, position, Quaternion.identity);
+			_targets.Add(target);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
