5815b5ebfbf9a63e15951483ce8d2a493b463763
Philipp Serrer <philipp@serrer.de>
2017-02-02 16:03:25 +0100

    SimpleCameraController Script for Camerea Movements with Keyboard

---
+++ Assets/Scripts/SimpleCameraController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCameraController : MonoBehaviour
+{
+
+	public float Speed = 1f;
+
+	private Camera _camera;
+
+	// Use this for initialization
+	void Start ()
+	{
+		_camera = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		var horizontal = Input.GetAxis("Horizontal");
+		if (Math.Abs(horizontal) > 0.01)
+		{
+			_camera.transform.Rotate(Vector3.up, Time.deltaTime * Speed * horizontal);
+		}
+	}
+}
